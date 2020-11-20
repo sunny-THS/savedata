c99c1540b307bd434a77639da6dc8a9e8554291c
sunny-THS <tuhueson@gmail.com>
2020-11-20 19:24:58 +0700

    sd

--- client/index.js
+++ client/index.js
@@ -5,37 +5,17 @@ socket.on('data', (res) => {
   while (ctr.hasChildNodes()) {
       ctr.removeChild(ctr.firstChild);
   }
-  console.log(res);
   res.forEach(data => {
     ShowData(data);
   });
 });
 socket.on('ServerSendData', (res) => {
   ShowData(res);
+  console.log(res);
+  document.querySelector('form').reset();
+  document.querySelector('span').textContent = 'Choose a file...'
 });
 
-function SendData(files) {
-  var counter = -1, file;
-  const date = new Date(Date.now());
-  while (file = files[++counter]) {
-    const lastModifiedDate_ = new Date(file.lastModified);
-    var reader = new FileReader();
-    reader.onloadend = (function(file) {
-      return function() {
-        const setupFile = {
-          name: file.name,
-          type: file.type,
-          date: date.toLocaleString('en-GB'),
-          lastModifiedDate: lastModifiedDate_.toLocaleString('en-GB'),
-          url_data: this.result
-        }
-        console.log(setupFile);
-        socket.emit('ClientSendData', setupFile);
-      }
-    })(file);
-    reader.readAsDataURL(file);
-  }
-}
 
 function newEl(type, attrs = {}) {
   const el = document.createElement(type);
@@ -72,30 +52,24 @@ function ShowData(val) {
 }
 
 
-var inputs = document.querySelectorAll('.inputfile');
-Array.prototype.forEach.call(inputs, function(input) {
-  var label = input.nextElementSibling,
-    labelVal = label.innerHTML;
+var inputs = document.querySelectorAll( '.inputfile' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+	var label	 = input.nextElementSibling,
+		labelVal = label.innerHTML;
 
-  input.addEventListener('change', function(e) {
-    const bol = confirm("Do you save your files?");
-    if (bol) {
-      console.log('success');
-      SendData(this.files);
-    } else {
-      console.log('exit');
-    }
-    e.target.value = '';
+	input.addEventListener( 'change', function( e )
+	{
+		var fileName = '';
+		if( this.files && this.files.length > 1 )
+			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+		else
+			fileName = e.target.value.split( '\\' ).pop();
 
-    var fileName = '';
-    if (this.files && this.files.length > 1)
-      fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-    else
-      fileName = e.target.value.split('\\').pop();
-
-    if (fileName)
-      label.querySelector('span').innerHTML = fileName;
-    else
-      label.innerHTML = labelVal;
-  });
+		if( fileName )
+			label.querySelector( 'span' ).innerHTML = fileName;
+		else
+			label.innerHTML = labelVal;
+    document.querySelector('.submit_').disabled = false;
+	});
 });
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const monk = require('monk');
+const fileUpload = require('express-fileupload')
 var http = require('http');
 if (process.env.NODE_ENV !== 'production')
   require('dotenv').config()
@@ -11,12 +12,12 @@ const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 const uri = process.env.MONGOURI;
 const db = monk(uri);
-const data = db.get('creators');
-data.options.castIds = false;
-// data.remove();
+const uploadFile = db.get('uploadFile');
+// uploadFile.remove();
 server.listen(port, () => console.log(`Example app listening on port ${port}`));
 
 app.use(express.static('client'));
+app.use(fileUpload())
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", '*'); // disabled for security local
@@ -32,17 +33,40 @@ app.get('/data', (req, res) => {
     })
 })
 
+app.post('/upload', (req, res) => {
+  var data;
+  const file_ = req.files.file;
+  const date = new Date(Date.now());
+  if (file_.length === undefined) {
+    data = {
+      name: file_.name,
+      type: file_.mimetype,
+      date: date.toLocaleString('en-GB'),
+      url_data: `data:${file_.mimetype};base64,${file_.data.toString('base64')}`
+    }
+  }else {
+    file_.forEach(file => {
+      data = {
+        name: file.name,
+        type: file.mimetype,
+        date: date.toLocaleString('en-GB'),
+        url_data: `data:${file.mimetype};base64,${file.data.toString('base64')}`
+      }
+    });
+  }
+  uploadFile.insert(data)
+    .then(res => io.sockets.emit('ServerSendData', res))
+    .catch(err => console.log(err));
+  res.redirect('/');
+})
+
 io.on('connection', (socket) => {
-  socket.on('ClientSendData', (data_) => {
-    data.insert(data_);
-    io.sockets.emit('ServerSendData', data_);
-  });
   socket.on('ClientRemoveData', (data_) => {
     data.remove();
     io.sockets.emit('ServerRemoveData', data_);
   });
 
-  data
+  uploadFile
   .find()
   .then(creator => {
     socket.emit('data', creator);
